d93be6163854ec91f6bb7232de8f3f91c73d7be9
JohnYsaac10 <sanz95pil@hotmail.com>
2020-10-11 18:44:39 -0500

    final

---
+++ SRIconciliacion/Models/Result.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SRIconciliacion.Models
+{
+    public class Result
+    {
+        public string Data { get; set; }
+    }
+}
